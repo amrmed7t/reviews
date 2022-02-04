4fb46321d555b3d2841bc7016781a7016180cf12
amr medhat <amr.medhat@icloudit.com>
2022-02-04 03:09:03 +0200

    first commit

---
+++ backend/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'c2e978a72b2abb6e46784195154b0eb7'),
+    },
+  },
+});
---
+++ backend/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '1e442f51-893e-4b57-872e-e48208f62fed'
+};
